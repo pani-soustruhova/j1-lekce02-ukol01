b169498433d2b90da17c48a7e5c0d870b99548f5
pani-soustruhova <pavlina@struzsky.cz>
2022-04-17 22:37:46 +0200

    prasátko

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -8,6 +8,44 @@ public class HlavniProgram {
         Turtle zofka;
 
         zofka = new Turtle();
+        nakresliPrasatko(zofka);
     }
 
+    public void nakresliPrasatko(Turtle zofka){
+        zofka.move(100);
+        zofka.turnLeft(120);
+        zofka.move(100);
+        zofka.turnLeft(120);
+        zofka.move(100);
+        // tělo
+        zofka.turnLeft(30);
+        zofka.move(200);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(200);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        //přední nožičky
+        zofka.turnRight(30);
+        zofka.move(50);
+        zofka.turnRight(180);
+        zofka.move(50);
+        zofka.turnRight(120);
+        zofka.move(50);
+        zofka.turnRight(180);
+        zofka.move(50);
+        zofka.turnRight(120);
+        zofka.move(200);
+        //zadní nožičky
+        zofka.turnRight(120);
+        zofka.move(50);
+        zofka.turnRight(180);
+        zofka.move(50);
+        zofka.turnRight(120);
+        zofka.move(50);
+        zofka.turnRight(180);
+        zofka.move(50);
+        zofka.turnRight(120);
+    }
 }
